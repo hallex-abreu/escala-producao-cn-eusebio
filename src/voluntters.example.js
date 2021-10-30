304d2e3b02db29d3e531eb1d076ea12103843498
jose.abreu <jose.abre@assai.com.br>
2021-10-30 18:26:09 -0300

    first commit

---
+++ src/voluntters.example.js
@@ -0,0 +1,57 @@
+//Domingo 10:00 = 1
+//Domingo 17:00 = 2
+//Quinta 19:30 = 3
+
+export default [
+    {
+        peoples: [
+            {name: 'name-1'},
+            {name: 'name-2'},
+        ],
+        days: [
+            1, 3
+        ]
+    },
+    {
+        peoples: [
+            {name: 'name-3'},
+            {name: 'name-4'}
+        ],
+        days: [
+            1
+        ]
+    },
+    {
+        peoples: [
+            {name: 'name-5'}
+        ],
+        days: [
+            2
+        ]
+    },
+    {
+        peoples: [
+            {name: 'name-5'}
+        ],
+        days: [
+            1, 2, 3
+        ]
+    },
+    {
+        peoples: [
+            {name: 'name-6'}
+        ],
+        days: [
+            1, 2, 3
+        ]
+    },
+    {
+        peoples: [
+            {name: 'name-7'},
+            {name: 'name-8'}
+        ],
+        days: [
+            1, 2, 3
+        ]
+    }
+]
